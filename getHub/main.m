6ee2a3c80dddb7add74eb9d38477327a144ab63b
lovelove1 <807602063@qq.com>
2016-04-27 16:26:19 +0800

    撒旦
    撒旦

---
+++ getHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  getHub
+//
+//  Created by MS on 16-4-27.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ getHubTests/getHubTests.m
@@ -0,0 +1,40 @@
+//
+//  getHubTests.m
+//  getHubTests
+//
+//  Created by MS on 16-4-27.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface getHubTests : XCTestCase
+
+@end
+
+@implementation getHubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
